feb0609afea2b3a45f59a702ddf7988f5e8eb026
David Taylor <tinystatemachine@gmail.com>
2020-02-17 15:58:06 +0000

    use single map of controllers for conn and state

--- app.js
+++ app.js
@@ -25,96 +25,139 @@ const DPS = {
   CUR_TEMP: '116' // tenths of deg.
 }
 
-const connections = {};
-const controllers = {};
+const devices = {};
 
-const start = function (name) {
-  if (!(name in connections)) {
-    console.log("creating connection to", name, "using", config.devices[name]);
-    connections[name] = new TuyAPI(config.devices[name]);
+
+const handleData = function (name, raw) {
+  data = raw['dps'];
+
+  state = devices[name].state;
+
+  let changed = false;
+
+  if (DPS.STATUS in data) {
+    console.log(name, "status: ", data[DPS.STATUS]);
+    state.status = data[DPS.STATUS];
+    changed = true;
   }
-  dev = connections[name];
-  console.log(name, "searching...");
-  dev.find().then(() => {
-    console.log(name, "connecting...");
-    dev.connect();
-  });
 
-  dev.on('connected', () => { console.log(name, "connected!"); });
-  dev.on('disconnected', () => {
-    console.log(name, "disconnected!");
-    delete controllers[name];
-  });
-
-  dev.on('error', error => { console.log(name, ' error:', error); });
-  dev.on('data', raw => {
-    data = raw['dps'];
-
-    if (!(name in controllers)) {
-      controllers[name] = {};
-    }
-    state = controllers[name];
-
-    let changed = false;
-
-    if (DPS.STATUS in data) {
-      console.log(name, "status: ", data[DPS.STATUS]);
-      state.status = data[DPS.STATUS];
+  if (DPS.CUR_TEMP in data) {
+    new_temp = data[DPS.CUR_TEMP] / 10.0;
+    if (state.temp != new_temp) {
+      console.log(name, "Temp changed: ", state.temp, " -> ", new_temp);
       changed = true;
     }
+    state.temp = new_temp;
+  }
 
-    if (DPS.CUR_TEMP in data) {
-      new_temp = data[DPS.CUR_TEMP] / 10.0;
-      if (state.temp != new_temp) {
-        console.log(name, "Temp changed: ", state.temp, " -> ", new_temp);
-        changed = true;
-      }
-      state.temp = new_temp;
-    }
+  let updateSP2 = false;
+  if (DPS.SP1 in data) {
+    new_sp1 = data[DPS.SP1] / 10.0;
+    if (state.sp1 != new_sp1) {
+      // Before updating set-points, check if sp2 was sp1+1.
+      updateSP2 = (state.sp2 - state.sp1) < 1.1;
 
-    if (DPS.SP1 in data) {
-      new_sp1 = data[DPS.SP1] / 10.0;
-      if (state.sp1 != new_sp1) {
-        console.log(name, "SP1 changed: ", state.sp1, " -> ", new_sp1);
-        changed = true;
-      }
-      state.sp1 = new_sp1;
+      console.log(name, "SP1 changed: ", state.sp1, " -> ", new_sp1);
+      changed = true;
     }
+    state.sp1 = new_sp1;
+  }
 
-    if (DPS.SP2 in data) {
-      new_sp2 = data[DPS.SP2] / 10.0;
-      if (state.sp2 != new_sp2) {
-        console.log(name, "SP2 changed: ", state.sp2, " -> ", new_sp2);
-        changed = true;
-      }
-      state.sp2 = new_sp2;
+  if (DPS.SP2 in data) {
+    new_sp2 = data[DPS.SP2] / 10.0;
+    if (state.sp2 != new_sp2) {
+      console.log(name, "SP2 changed: ", state.sp2, " -> ", new_sp2);
+      changed = true;
     }
+    state.sp2 = new_sp2;
+  }
 
-    if (DPS.ACTIVE in data) {
-      new_cooling = data[DPS.ACTIVE];
-      if (state.cooling != new_cooling) {
-        console.log(name, "Status changed: cooling ", state.cooling, " -> cooling ", new_cooling);
-        changed = true;
-      }
-      state.cooling = data[DPS.ACTIVE];
+  // If sp2 was sp1+1 before but isn't now,
+  if (config.auto_sp2 !== false && updateSP2) {
+    let target = state.sp1 + 1.0;
+    console.log(name, "setting sp2 to sp1+1", target);
+    devices[name].conn.set({ dps: DPS.SP2, set: target * 10.0 }).then(() => {
+      console.log(name, "set sp2 to ", target);
+    }, (reason) => {
+      console.log(name, "failed to set sp2", reason);
+    })
+  }
+
+
+  if (DPS.ACTIVE in data) {
+    new_cooling = data[DPS.ACTIVE];
+    if (state.cooling != new_cooling) {
+      console.log(name, "Status changed: cooling ", state.cooling, " -> cooling ", new_cooling);
+      changed = true;
     }
-    if (changed) {
-      state.changed = true;
-    }
-    state.updated = new Date();
-  })
+    state.cooling = data[DPS.ACTIVE];
+  }
+  if (changed) {
+    state.changed = true;
+  }
+  state.updated = new Date();
 }
 
-const loop = function(name) {
+const start = function (name) {
+  if (!(name in devices)) {
+    console.log("creating connection to", name, "using", config.devices[name]);
+    dev = new TuyAPI(config.devices[name]);
+    devices[name] = { "conn": dev };
+
+    dev.on('connected', () => {
+      devices[name].state = { "status": "connected" };
+      console.log(name, "connected!");
+    });
+    dev.on('disconnected', () => {
+      console.log(name, "disconnected!");
+      devices[name].state = {};
+      delete devices[name]["connecting"];
+
+    });
+    dev.on('error', error => { console.log(name, ' error:', error); });
+    dev.on('data', raw => { handleData(name, raw) });
+  }
+
+  if (devices[name].conn.isConnected()) {
+    return
+  }
+
+  if (!devices[name].connecting) {
+    devices[name].connecting = true;
+    devices[name].conn.find().then((found) => {
+      if (!found) {
+        console.log("device not found")
+        return
+      }
+      console.log(name, "connecting...");
+      devices[name].conn.connect().then((connected) => {
+        delete devices[name]["connecting"];
+        if (!connected) {
+          console.log(name, "connection failed");
+        }
+      }).catch((reason) => {
+        delete devices[name]["connecting"];
+        console.log(name, "connection failed", reason);
+      });
+    }).catch((reason) => {
+      delete devices[name]["connecting"];
+      console.log(name, "not found", reason);
+    });
+  }
+
+}
+
+
+const loop = function (name) {
+  start(name);
+
   setInterval(() => {
-    if (!(name in controllers)) {
+    if (!("state" in devices[name]) || !("status" in devices[name].state)) {
       console.log(name, "not connected?")
       start(name);
       return
     }
-
-    state = controllers[name];
-
+    state = devices[name].state;
     if (state.status != 'on') {
       return
     }
@@ -160,7 +203,16 @@ for (var name in config.devices) {
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(controllers, null, "  "));
+  let state = {};
+  for (var name in devices) {
+    state[name] = {};
+    for (var key in devices[name]) {
+      if (key != "conn") {
+        state[name][key] = devices[name][key];
+      }
+    }
+  }
+  res.end(JSON.stringify(state, null, "  "));
 });
 
 server.listen(port, () => {
